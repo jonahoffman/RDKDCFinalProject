168d93059decca226b503ad3b92190e5c82b7729
Jonah Offman <joffman1@jhu.edu>
2021-12-12 19:26:21 -0500

    framework for all control algos

---
+++ ur5_project.m
@@ -0,0 +1,30 @@
+% UR5 Stuff
+ur5 = ur5_interface();
+home = [0 0 0 0 0 0]; % FIX
+ur5.move_joints(home, 10);
+pause(10); 
+
+gst_1 = [0 -1 0 0.3; -1 0 0 -0.4; 0 0 -1 0.22; 0 0 0 1];
+gst_2 = [0 -1 0 -0.3; -1 0 0 0.39; 0 0 -1 0.22; 0 0 0 1];
+
+% Robot at home configuration. 
+% Robot moves so the EE frame goes to pose directly above start location.
+% Robot moves straight down to the start location.
+% Robot moves back to initial location (above inital).
+% Robot moves above the target location. 
+% Robot moves down to end location.
+% Robot moves back to above the target location.
+
+type = input("Choose algorithm type: IK, DK, or gradient"); 
+
+if strcmpi(type, "IK")
+    ur5IKcontrol(gst_1, gst_2, ur5); 
+elseif strcmpi(type, "DK")
+    K = 0.25;
+    run_ur5RRcontrol(gst_1, gst_2, K, ur5);
+elseif strcmpi(type, "gradient")
+    return
+else
+    disp("Invalid algorithm type. Options are: 'IK, DK, or gradient.'");
+end
+    
